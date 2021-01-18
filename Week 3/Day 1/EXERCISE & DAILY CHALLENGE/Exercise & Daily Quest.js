63fb9f7a502a8569859fa524fa6a2daad4878300
Sasha <deadsnake555@gmail.com>
2021-01-18 10:46:13 +0200

    Update

---
+++ Week 3/Day 1/EXERCISE & DAILY CHALLENGE/Exercise & Daily Quest.js
@@ -0,0 +1,35 @@
+//Exercise 1
+
+//5 + "34" = "534";
+//5 - "4"= "1";
+//10 % 5 = 0;
+//"Java" + "Script"= "Javascript";
+//   " "+" " = " ";
+ //   " " + 0 = "0";
+ //   true + true = 2;
+ //   true + false = 1;
+ //   false + true = 1;
+ //   false - true = 0;
+ //   3 - 4 = -1;
+ //   "Bob" + "bill" = BobBill;
+
+//Exercise 2
+
+
+// function func() {  
+//    var me = [ "my", "favorite", "color", "is", "blue"];  
+//    document.write(me.join(' '));  
+// }  
+// func();  
+
+
+// Daily Challenge
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. console.log(fruits.shift());
+// 2. alert("fruit.sort()");
+// 3. console.log(fruits.push("Kiwi"));
+// 4. var fruitWithoutApple = fruits.slice(0,1);
+//    console.log(fruits);
+// 5.console.log(fruits.reverse());   
---
+++ Week 3/Day 1/EXERCISE & DAILY CHALLENGE/script.js
@@ -0,0 +1,29 @@
+
+console.log('Yeah');
+let name = "Sasha";
+
+console.log('My name is' + ' ' + name);
+
+var x;
+
+x=3
+
+var y, z;
+
+y = 5 + 6;
+
+z = y * 10;
+
+console.log(y+z);
+
+var sasha = prompt('input');
+//console.log('sasha');
+
+var age = prompt('How old are you?');
+//console.log(age);
+
+function avg(number1, number2){
+    return (number1 + number2) / 2
+}
+
+avg();
