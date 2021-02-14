8f0f7c34f3098d4c0b78351519201eb1c8b44c52
Sasha <deadsnake555@gmail.com>
2021-02-14 14:45:24 +0200

    Update

--- Week 6/Day 5/Daily Challene.py
+++ Week 6/Day 5/Daily Challene.py
@@ -1,5 +1,3 @@
-
-
 word = input('Enter a word\n')
 encrypt = input("Do you want to encrypt ?\n")
 shift = int(input("what shift do you want to use for encryption ?\n"))
@@ -7,13 +5,13 @@ final_word = ''
 
 
 if encrypt == "yes":
-    for letter in word:
-    num = ord(letter)
-    let = chr(num + shift)
-    final_word += let
-
+        for letter in word:
+            num = ord(letter)
+            let = chr(num + shift)
+            final_word += let
 else:
-    print("okay")
+        print("okay")
+
 
 print(f"The encrypted word is {final_word}")
 decrypt = input("do you want to decrypt ?\n")
